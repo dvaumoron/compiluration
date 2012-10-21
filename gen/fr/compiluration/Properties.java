046c4efe595e5f2968e151ae85ca08caf38cabb9
Denis Vaumoron <dvaumoron@gmail.com>
2012-10-21 20:07:02 +0200

    fichier généré

---
+++ gen/fr/compiluration/Properties.java
@@ -0,0 +1,13 @@
+package fr.compiluration;
+
+public final class Properties {
+	public static final class un {
+		public static final int entier = 1;
+		public static final double reel = 3.14159265358979;
+	}
+	public static final class une {
+		public static final class propriete {
+			public static final String complexe = "bla";
+		}
+	}
+}
